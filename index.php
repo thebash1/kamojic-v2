063af322a45a221069ca8d15f489c0fc534bc38b
thebash1 <alex.daniel50904@gmail.com>
2024-12-03 10:27:03 -0500

    agregando indice

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index of Kamojic</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .container{
+            margin: 5rem 0 5rem 5rem;
+        }
+        h1{
+            text-align: start;
+            margin-bottom: 3rem;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Index of Kamojic</h1>
+        <a href="./public/login.php">Ir a login</a>
+        <a href="./public/register.php">Ir a registro</a>
+        <a href="./config/database.php">Conexión base de datos</a>
+    </div>
+</body>
+</html>
