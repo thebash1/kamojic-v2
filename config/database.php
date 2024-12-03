e1aeca80f169ea280ae874f67ed815fe79f60986
thebash1 <alex.daniel50904@gmail.com>
2024-12-02 22:01:20 -0500

    conectando base de datos

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+    $host = "sql311.infinityfree.com";
+    $username = "if0_37810355";
+    $password = "1HhFx7F57ie";
+    $dbname = "if0_37810355_db_kamojic";
+    $port = 3306;
+    
+    try{
+        $conn = new mysqli($host, $username, $password,$dbname,$port);
+        if($conn->connect_error){
+            throw new Exception("error en conexión <br>", $conn->connect_error);
+        }
+        echo "conexión establedia <br>";
+
+    } catch(Exception $e){
+        echo "<script>alert('base de datos -> conexión fallida')</script><br><br>" . $e->getMessage() . '<br>';
+        
+    }
